b46e8d55bd85bc4c2fe4952b5ecc10fbcfdbf392
Travis Whitaker <pi.boy.travis@gmail.com>
2019-04-22 23:39:58 +0000

    Deps broken for frames, so do them in topo order.

--- emacs.el
+++ emacs.el
@@ -32,11 +32,11 @@
 (use-package magit :ensure t)
 (use-package quelpa :ensure t)
 (use-package quelpa-use-package :ensure t)
-(use-package frame-cmds
-             :quelpa (frame-cmds :fetcher github :repo "emacsmirror/frame-cmds")
-)
 (use-package frame-fns
              :quelpa (frame-fns :fetcher github :repo "emacsmirror/frame-fns")
+)
+(use-package frame-cmds
+             :quelpa (frame-cmds :fetcher github :repo "emacsmirror/frame-cmds")
 )
 (use-package zoom-frm
              :quelpa (zoom-frm :fetcher github :repo "emacsmirror/zoom-frm")
@@ -137,3 +137,17 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (frame-fns rainbow-delimiters quelpa-use-package nix-mode markdown-mode magit haskell-mode general evil dhall-mode column-enforce-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
