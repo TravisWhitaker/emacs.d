49ee5e6177b6bc98f803cde37dbebba300e249af
Travis Whitaker <pi.boy.travis@gmail.com>
2019-04-23 00:16:49 +0000

    Use c-mode on .cmm files.

--- emacs.el
+++ emacs.el
@@ -126,6 +126,9 @@
      'haskell-compile)
 )
 
+; Cmm stuff:
+(add-to-list 'auto-mode-alist '("\\.cmm\\'" . c-mode))
+
 (setq-default haskell-indentation-layout-offset 4)
 (setq-default haskell-indentation-left-offset 4)
 (setq-default haskell-indentation-starter-offset 4)
@@ -137,17 +140,3 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (frame-fns rainbow-delimiters quelpa-use-package nix-mode markdown-mode magit haskell-mode general evil dhall-mode column-enforce-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
